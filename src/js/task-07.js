e81a60887f1eb33d52866ead0be4a08194bd1434
StanislavPavlenko <voltfrag@gmail.com>
2023-05-14 20:29:39 +0300

    task

--- src/js/task-07.js
+++ src/js/task-07.js
@@ -3,5 +3,15 @@
 // <input id="font-size-control" type="range" min="16" max="96" />
 // <br />
 // <span id="text">Abracadabra!</span>
+const refs = {
+    sizeEl: document.querySelector('#font-size-control'),
+    span: document.querySelector('#text')
+}
+console.log(refs.sizeEl)
+refs.span.style.fontSize = `${refs.sizeEl.value}px`;
 
+function sizeFontEl() {
+    return (refs.span.style.fontSize = `${refs.sizeEl.value}px`);
+}
 
+refs.sizeEl.addEventListener('input', sizeFontEl);
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,24 @@
+// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
+
+// <div class="widget">
+//   <p>Background color: <span class="color">-</span></p>
+//   <button type="button" class="change-color">Change color</button>
+// </div>
+
+// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
+
+const refs = {
+  widgetEl: document.querySelector('.widget'),
+  colorEl: document.querySelector('.color'),
+  changeColorEl: document.querySelector('.change-color'),
+}
+
+refs.changeColorEl.addEventListener('click', () => {
+  const color = getRandomHexColor();
+  refs.colorEl.textContent = color;
+  refs.widgetEl.style.backgroundColor = color;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
