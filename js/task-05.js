096b52184d778014c3964cb6f21b3ea49e973757
StanislavPavlenko <voltfrag@gmail.com>
2023-05-17 16:13:08 +0300

    ggg

--- js/task-05.js
+++ js/task-05.js
@@ -1,8 +1,3 @@
-// Напиши скрипт, який під час набору тексту в інпуті input#name-input (подія input), підставляє його поточне значення в span#name-output. Якщо інпут порожній, у спані повинен відображатися рядок "Anonymous".
-
-// <input type="text" id="name-input" placeholder="Please enter your name" />
-// <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
-
 const refs = {
     input: document.querySelector('#name-input'),
     span: document.querySelector('#name-output'),
@@ -14,6 +9,6 @@ function onInput(event) {
     refs.span.textContent = event.currentTarget.value;
 
     if (event.currentTarget.value === "") {
-    span.textContent = "Anonymous";
+    refs.span.textContent = "Anonymous";
   }
 }
--- js/task-09.js
+++ js/task-09.js
@@ -1,12 +1,3 @@
-// Напиши скрипт, який змінює кольори фону елемента <body> через інлайн-стиль по кліку на button.change-color і виводить значення кольору в span.color.
-
-// <div class="widget">
-//   <p>Background color: <span class="color">-</span></p>
-//   <button type="button" class="change-color">Change color</button>
-// </div>
-
-// Для генерування випадкового кольору використовуй функцію getRandomHexColor.
-
 const refs = {
   widgetEl: document.querySelector('.widget'),
   colorEl: document.querySelector('.color'),
